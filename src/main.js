42da36ca7492d07ee2d4c7fed18fa655e58eb4ca
yamabuki-chan <1697130+yamule@users.noreply.github.com>
2019-09-08 12:03:41 +0900

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow,Menu,ipcMain} = require("electron");
+let mainWindow;
+const template = [
+  {
+      label: 'Object',
+      submenu: [
+          {
+              label: 'Load',
+              click () { mainWindow.webContents.send("load_files",["test"]); }
+          },
+          {
+              label: 'Clear',
+              click () { mainWindow.webContents.send("clear_objects");}
+          }
+      ]
+  }
+]
+
+const menu = Menu.buildFromTemplate(template);
+Menu.setApplicationMenu(menu);
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    },
+    width: 800,
+    height: 600
+  });
+  mainWindow.loadFile("loader_test.html");
+  //mainWindow.webContents.openDevTools();
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
